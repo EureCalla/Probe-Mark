f21b2eb6033d64d3ff5997ca89764ee74b3b9ff9
Della <113402238+EureCalla@users.noreply.github.com>
2024-08-10 16:31:18 +0800

    將mask圖存成三個通道，label圖變成純粹的0,1 label

--- probe_mark/dataset_Della/get_img.py
+++ probe_mark/dataset_Della/get_img.py
@@ -97,7 +97,7 @@ def make_folder(path, folder_name, date=None):
     return os.path.abspath(full_path)
 
 
-def get_img_label(image_path1, image_path2, save_path):
+def get_img_mask(image_path1, image_path2, save_path):
     try:
         # 讀取圖像並轉換為RGBA格式
         image1 = Image.open(image_path1).convert("RGBA")
@@ -118,7 +118,7 @@ def get_img_label(image_path1, image_path2, save_path):
 
         # 創建一個新圖像，將不同的區域變為白色，其他區域變為黑色
         highlight_image_array_black = np.where(
-            difference_array[..., None], [255, 255, 255, 255], [0, 0, 0, 255]
+            difference_array[..., None], [255, 255, 255], [0, 0, 0]
         )
 
         # 將結果轉換回PIL圖像
@@ -128,7 +128,40 @@ def get_img_label(image_path1, image_path2, save_path):
 
         # 保存結果圖像
         highlight_image_black.save(save_path)
-        # print(f"Image saved to {save_path}")
+        print(f"Image saved to {save_path}")
+
+    except ValueError as ve:
+        print(f"{image_path1}和{image_path2}發生圖片大小不一致: {ve}")
+
+
+def get_img_label(image_path1, image_path2, save_path):
+    try:
+        # 讀取圖像並轉換為RGBA格式
+        image1 = Image.open(image_path1).convert("RGBA")
+        image2 = Image.open(image_path2).convert("RGBA")
+
+        # 檢查兩個圖像的大小是否一致
+        if image1.size != image2.size:
+            raise ValueError("圖片大小不一致")
+
+        # 將圖像轉換為numpy數組
+        image1_array = np.array(image1)
+        image2_array = np.array(image2)
+
+        # 找到兩個圖像之間RGB不一樣的區域
+        difference_array = np.any(
+            image1_array[:, :, :3] != image2_array[:, :, :3], axis=-1
+        )
+
+        # 創建一個新圖像，將不同的區域變為255，其他區域變為0
+        label_image_array = np.where(difference_array, 1, 0).astype(np.uint8)
+
+        # 將結果轉換回PIL圖像
+        label_image = Image.fromarray(label_image_array, mode="L")
+
+        # 保存結果圖像
+        label_image.save(save_path)
+        print(f"Image saved to {save_path}")
 
     except ValueError as ve:
         print(f"{image_path1}和{image_path2}發生圖片大小不一致: {ve}")
