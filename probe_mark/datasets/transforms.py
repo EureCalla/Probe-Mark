1819762756da2fd96448428f682e5644072a619d
hcchen-moraleai <hcchen.nick@gmail.com>
2024-08-16 20:54:58 +0800

    refactor: Update transforms.py with torchvision.transforms.v2 and torchvision.tv_tensors

--- probe_mark/datasets/transforms.py
+++ probe_mark/datasets/transforms.py
@@ -1,26 +1,42 @@
 import torch
-from torchvision.transforms import v2
+import torchvision.transforms.v2 as T
+import torchvision.tv_tensors as tv_tensors
 
-BASIC_TRANSFORMS = v2.Compose(
+BASIC_TRANSFORMS = T.Compose(
     [
-        v2.ToImage(),
-        v2.Pad((0, 129, 0, 129), fill=0, padding_mode="constant"),
-        v2.Resize(size=(256, 256)),
-        v2.ToDtype(torch.float32),
-        # v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        T.ToImage(),
+        T.Pad(padding=(0, 129, 0, 129), fill=0, padding_mode="constant"),
+        T.Resize(size=(256, 256)),
+        T.ToDtype(
+            dtype={
+                tv_tensors.Image: torch.float32,
+                tv_tensors.Mask: torch.int64,
+                "others": None,
+            },
+            scale=True,
+        ),
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ]
 )
 
-AUGMENTATION_TRANSFORMS = v2.Compose(
+AUGMENTATION_TRANSFORMS = T.Compose(
     [
-        v2.ToImage(),
-        v2.Pad((0, 129, 0, 129), fill=0, padding_mode="constant"),
-        v2.Resize(size=(256, 256)),
-        v2.RandomHorizontalFlip(p=0.5),
-        v2.RandomVerticalFlip(p=0.5),
-        v2.RandomAdjustSharpness(sharpness_factor=4, p=0.5),
-        v2.RandomAutocontrast(p=0.5),
-        v2.ToDtype(torch.float32, scale=True),
-        # v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        T.ToImage(),
+        T.Pad(padding=(0, 129, 0, 129), fill=0, padding_mode="constant"),
+        T.Resize(size=(256, 256)),
+        T.RandomHorizontalFlip(p=0.5),
+        T.RandomVerticalFlip(p=0.5),
+        T.RandomAdjustSharpness(sharpness_factor=4, p=0.5),
+        T.RandomAutocontrast(p=0.5),
+        T.ToDtype(
+            dtype={
+                tv_tensors.Image: torch.float32,
+                tv_tensors.Mask: torch.int64,
+                "others": None,
+            },
+            scale=True,
+        ),
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        T.ToPureTensor(),
     ]
 )
